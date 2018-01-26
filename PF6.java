fc3eacccacb87788ace68a26c3b68241b97b88b7
joaolucas0516 <32168994+joaolucas0516@users.noreply.github.com>
2018-01-26 17:29:52 +0000

    Add files via upload

---
+++ PF6.java
@@ -0,0 +1,18 @@
+package notasdosalunos;
+import java.util.Scanner;
+public class PF6 {
+	static int lerescrevernota (int nota) { 
+		Scanner LerS = new Scanner (System.in);
+		System.out.print("Qual a nota do aluno?");
+		notafinal=LerS.nextInt();
+		return notafinal;}
+	public static void main(String[] agrs) throws Exception{
+		int notafinal = 0;
+		int [] Armario = new int [26];
+				for (Int aluno = 1; notafinal < 26; aluno++);
+		Armario [aluno] = Lerescrevernota(notafinal);
+		for (int aluno = 1;notafinal<26; alunoo++);
+		System.out.println(aluno +"-"+Armario[aluno]);
+	}
+
+}
