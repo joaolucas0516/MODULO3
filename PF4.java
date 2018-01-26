49e750e0646693165198b7a7642c573e7f180674
joaolucas0516 <32168994+joaolucas0516@users.noreply.github.com>
2018-01-26 15:58:42 +0000

    Add files via upload

---
+++ PF4.java
@@ -0,0 +1,22 @@
+package MAIORDE3;
+import java.util.Scanner;
+public class PF4 {
+	static int MAIORDE3 (int X, int Y, int Z)
+		    { if (X<Y)
+	        X=Y;
+			 if (X<Z)
+			X=Z;
+			return X;}
+		public static void main (String [] args) throws Exception{
+		 int X,Y,Z;
+	Scanner LerS = new Scanner (System.in);
+	System.out.print ("Digite o primeiro valor (X)");
+	X = LerS.nextInt ();
+	System.out.print ("Digite o segundo valor (Y)");
+	Y = LerS.nextInt ();
+	System.out.print ("Digite o terceiro valor (Z)");
+	Z = LerS.nextInt ();
+	System.out.println("Maior =" + MAIORDE3 (X, Y, Z));
+					}
+		    }
+		
